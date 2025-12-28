767116b841ccd2053d5f9f0a8fbce41a128e433e
Abhay Vishwakarma <mrabhay88lk@gmail.com>
2025-12-28 15:01:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ import time
 from sklearn.datasets import fetch_california_housing
 
 st.title('🏠 House Price prediction using ML')
-st.image('https://i.pinimg.com/originals/93/c7/44/93c744bcde1780c94bb1d3f03991f8a7.gif'
+st.image('https://i.pinimg.com/originals/93/c7/44/93c744bcde1780c94bb1d3f03991f8a7.gif')
+
